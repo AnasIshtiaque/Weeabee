ac4d1665b3f2c5ac2a0cfdda27ad5215abfde616
Waleed Parwez <wparwez@uwaterloo.ca>
2020-05-16 17:15:44 -0400

    Achievements added

---
+++ app/src/main/java/com/example/weeabee/Achievements.java
@@ -0,0 +1,28 @@
+package com.example.weeabee;
+
+public class Achievements {
+
+
+    public void chef_achieve() {
+    }
+
+    String[] c_achieve = new String[] {"Aspiring Chef", "Green Chef", "Blue Chef", "Bronze Chef", "Bronze Chef", "Silver Chef", "Gold Chef", "Platinum Chef",  "Diamond Chef", "Master Chef" };
+    private int chefcounter=0;  // counts the amount of meals cooked
+
+    public void chef_incrementer()
+    { // to increment the counter
+
+
+
+    }
+
+    public void showachivievement (){
+        // to show the achievement
+
+
+
+
+
+
+    }
+}
