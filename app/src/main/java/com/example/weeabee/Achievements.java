baa64d212a251cf3f7ebba25546f6de751ef8e57
Anas-Ishtiaque <anas.ishtiaque@outlook.com>
2020-05-16 17:23:56 -0400

    achievements updated

--- app/src/main/java/com/example/weeabee/Achievements.java
+++ app/src/main/java/com/example/weeabee/Achievements.java
@@ -2,27 +2,24 @@ package com.example.weeabee;
 
 public class Achievements {
 
-
     public void chef_achieve() {
-    }
-
-    String[] c_achieve = new String[] {"Aspiring Chef", "Green Chef", "Blue Chef", "Bronze Chef", "Bronze Chef", "Silver Chef", "Gold Chef", "Platinum Chef",  "Diamond Chef", "Master Chef" };
-    private int chefcounter=0;  // counts the amount of meals cooked
-
-    public void chef_incrementer()
-    { // to increment the counter
-
-
 
     }
 
-    public void showachivievement (){
-        // to show the achievement
+    String[] c_achieve = new String[] {"Aspiring Chef", "Green Chef", "Blue Chef", "Bronze Chef", "Bronze Chef", "Silver Chef", "Gold Chef", "Platinum Chef",  "Diamond Chef", "Master Chef"};
+    // The amounts of meals cooked
+    int mealsCooked=0;
 
-
-
-
-
+    // Increments the amount of meals the user has cooked
+    private void mealIncrementer()
+    {
 
     }
+
+    // Shows how many meals someone has cooked 
+    public void showAchivievement ()
+    {
+
+    }
+
 }
